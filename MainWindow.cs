6a9067d8a2ce9edd662c01ecb6f7bd55b8d96fb4
Adhir Ramjiawan <adhirramjiawan0@gmail.com>
2024-10-02 22:49:45 +0200

    initial commit

---
+++ MainWindow.cs
@@ -0,0 +1,27 @@
+using System;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+
+namespace csharp_ide
+{
+    class MainWindow : Window
+    {
+        [UI] private Label _label1 = null;
+
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            builder.Autoconnect(this);
+
+            DeleteEvent += Window_DeleteEvent;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+
+    }
+}
